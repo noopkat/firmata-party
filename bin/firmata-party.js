4f007cce7d2ea1a75508d5a71d60ef15966c25a4
Suz Hinton <suz.hinton@gmail.com>
2015-09-28 14:24:42 +0200

    initial commit

---
+++ bin/firmata-party.js
@@ -0,0 +1,42 @@
+#!/usr/bin/env node
+var Avrgirl = require('avrgirl-arduino');
+var parseArgs = require('minimist');
+var path = require('path');
+var supportedBoards = require('../node_modules/avrgirl-arduino/boards.js');
+var _ = require('underscore');
+
+var args = (process.argv.slice(2));
+var argv = parseArgs(args, opts={})
+var arduino = argv._[0];
+var help = 'usage: firmata-party <arduino name>';
+
+function showHelp() {
+  console.log(help);
+}
+
+handleInput(arduino);
+
+function handleInput(board) {
+  if (board && _.has(supportedBoards, board)) {
+    flash({board: board});
+  } else {
+    showHelp();
+    return process.exit(1);
+  }
+}
+
+function flash(options) {
+  var avrgirl = new Avrgirl(options);
+  var filepath = path.resolve(process.cwd(), '..', 'node_modules', 'avrgirl-arduino', 'junk', 'hex', options.board, 'StandardFirmata.cpp.hex');
+  console.log(filepath)
+
+  avrgirl.flash(filepath, function(error) {
+    if (error) {
+      console.error(error);
+      return process.exit(1);
+    } else {
+      return process.exit();
+    }
+  });
+
+}
