a1e033eff79a5da80acb1e2d3e5ba1c10833272a
Suz Hinton <suz.hinton@gmail.com>
2017-05-28 12:13:40 -0400

    add duemilanove328 to supported boards list

--- boards.js
+++ boards.js
@@ -68,6 +68,15 @@ var boards = {
     productId: ['0x6001'],
     protocol: 'stk500v1'
   },
+  'duemilanove328': {
+    baud: 57600,
+    signature: new Buffer([0x1e, 0x95, 0x14]),
+    pageSize: 128,
+    numPages: 256,
+    timeout: 400,
+    productId: ['0x6001'],
+    protocol: 'stk500v1'
+  },
   'tinyduino': {
     baud: 57600,
     signature: new Buffer([0x1e, 0x95, 0x0f]),
